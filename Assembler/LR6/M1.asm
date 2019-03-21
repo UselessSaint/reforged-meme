3458eb1236d1bb5d1e4a3046466507302300dac3
Podvashetsky Dmitry <furt@yandex.ru>
2019-03-22 02:45:50 +0300

    nothing

--- Assembler/LR6/M1.asm
+++ Assembler/LR6/M1.asm
@@ -1,8 +1,33 @@
 PUBLIC	M1
 
-CSEG SEGMENT byte PUBLIC 'DATA'
-ASSUME CS:CSEG
-M1:	ret
+DataMenu   SEGMENT BYTE PUBLIC 'DATA'
+	menu	db 13, 10
+			db "'0' Print menu.", 13, 10
+			db "'1' Enter number", 13, 10
+			db "'2' __________", 13, 10
+			db "'3' __________", 13, 10
+			db "'4' __________", 13, 10
+			db "'5' __________", 13, 10
+			db "'6' __________", 13, 10
+			db "'7' __________", 13, 10
+			db "'8' Exit.", 13, 10
+			db "Choose: "
+			db "$"
+DataMenu   ENDS
 
-CSEG ENDS
+ASSUME	CS:CSEG, DS:DataMenu
+
+CSEG	SEGMENT byte PUBLIC 'CODE'
+	M1:
+		;push AX
+		;push DX
+		
+		;mov DX, offset menu
+		;mov AH, 9
+		;int 21h
+		
+		;pop DX
+		;pop AX
+		ret
+CSEG	ENDS
 	END
--- Assembler/LR6/main.asm
+++ Assembler/LR6/main.asm
@@ -6,37 +6,42 @@ EXTRN M5: near
 EXTRN M6: near
 
 StkSeg  SEGMENT PARA STACK 'STACK'
-        DB      200h DUP (?)
+        DB      200h DUP (0)
 StkSeg  ENDS
-;
+
 DataS   SEGMENT BYTE PUBLIC 'DATA'
-	menu	db "'0' Print menu", 13, 10
-		db "'1' Print menu", 13, 10
-		db "'2' Print menu", 13, 10
-		db "Choose: ", 13, 10
-		db "$"
-		
 	OFFM 	dw M1, M2, M3, M4, M5, M6
-	N	db 0
-
+	N		db 0
 DataS   ENDS
-;
+
+ASSUME  CS:Code, DS:DataS, SS:StkSeg
 Code    SEGMENT BYTE PUBLIC 'CODE'
-        ASSUME  CS:Code, DS:DataS, SS:StkSeg
-main:
-        mov   AX,DataS
-        mov   DS,AX    
+        
+	main:
+		mov AX, DataS
+		mov DS, AX
+		call M1
+		
+	menu:
+		mov AH, 8
+		int 21h
+		mov AH, 2
+		mov DL, AL
+		int 21h
+		
+		cmp   AL, '8'                
+		JA    menu
+		CMP   AL, '0'
+		JB    menu
+		
+		mov BX, 0
+		mov BL, AL
+		sub BL, '0'
+		
+		call OFFM[BX]
 
-	mov DX, OFFSET menu
-	mov AH, 9
-	int 21h
-
-	mov BH, 0
-	mov BL, N
-	call OFFM[BX]
-
-	mov AH, 4Ch
-	int 21h
-
+	exit:
+		mov AH, 4Ch
+		int 21h
 Code    ENDS
         END   main
